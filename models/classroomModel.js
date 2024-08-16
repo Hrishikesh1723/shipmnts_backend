a70eb20b3a59ac412f700ec162d41990559e2c67
Hrishikesh1723 <hrishikesh.r@ahduni.edu.in>
2024-08-16 10:21:55 +0530

    models created

--- models/classroomModel.js
+++ models/classroomModel.js
@@ -1,51 +1,40 @@
 const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
 
-//Defining admin datatypes
-const teacherSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: true,
-  },
-  email: {
-    type: String,
-    require: true,
-  },
-  password: {
-    type: String,
-    require: true,
-  },
-  tokens: [
-    {
-      token: {
+const classroomSchema = new mongoose.Schema({
+    className: {
         type: String,
         require: true,
-      },
     },
-  ],
-});
+    teacherId: {
+        type: String,
+        require: true,
+    },
+    students: [
+        {
+            StudentId: {     
+                type: String,
+                require: true,
+            }
+        }
+    ],
+    tasks: [
+        {
+            title: {
+                type: String,
+                require: true,
+            },
+            description: {
+                type: String,
+                require: true,
+            },
+            duedate: {
+                type: Date,
+                require: true,
+            }
+        }
+    ]
+})
 
-// securing password
-teacherSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, 12);
-  }
-  next();
-});
+const Classroom = mongoose.model("Classroom", classroomSchemaSchema);
 
-// generating Auth token
-teacherSchema.methods.generateAuthToken = async function () {
-  try {
-    let token = jwt.sign({ _id: this._id }, process.env.KEY);
-    this.tokens = this.tokens.concat({ token: token });
-    await this.save();
-    return token;
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const Teacher = mongoose.model("ADMIN", teacherSchema);
-
-module.exports = Teacher;
+module.exports = Classroom;
---
+++ models/studentModel.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+//Defining admin datatypes
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  classrooms: [
+    {
+        classId: {
+            type: String,
+            require: true,
+        },
+        classname: {
+            type: String,
+            require: true,
+        }
+    }
+  ],
+  tokens: [
+    {
+      token: {
+        type: String,
+        require: true,
+      },
+    },
+  ],
+});
+
+// securing password
+studentSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  next();
+});
+
+// generating Auth token
+studentSchema.methods.generateAuthToken = async function () {
+  try {
+    let token = jwt.sign({ _id: this._id }, process.env.KEY);
+    this.tokens = this.tokens.concat({ token: token });
+    await this.save();
+    return token;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const Student = mongoose.model("Student", studentSchema);
+
+module.exports = Student;
